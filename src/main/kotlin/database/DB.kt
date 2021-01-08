6fd908bd7cfcdffefba957604a883df78e9a8bf3
shankarsawant81 <shankarsawant81@gmail.com>
2021-01-08 17:08:47 +0530

    After adding passions

--- src/main/kotlin/database/DB.kt
+++ src/main/kotlin/database/DB.kt
@@ -1,6 +1,7 @@
 package database
 
 import org.jetbrains.exposed.sql.Database
+import java.net.URI
 
 object DB {
     private val host: String
@@ -10,35 +11,29 @@ object DB {
     private val dbPassword: String
 
     init {
-        host ="localhost"
-        port = 5555
-        dbName = "tinderr_db"
-        dbUser = "tinderr_user"
-        dbPassword = "tinderrpass123"
+//        host ="localhost"
+//        port = 5555
+//        dbName = "tinderr_db"
+//        dbUser = "tinderr_user"
+//        dbPassword = "tinderrpass123"
 
-//        val dbUrl = System.getenv("DATABASE_URL")
-//
-//        if (dbUrl != null) {
-//            val dbUri = URI(dbUrl)
-//            host = dbUri.host
-//            port = dbUri.port
-//            dbName = dbUri.path.substring(1)
-//            val userInfo = dbUri.userInfo.split(":")
-//            dbUser = userInfo[0]
-//            dbPassword = userInfo[1]
-//        } else {
-//            host = System.getenv("DB_HOST")
-//            port = System.getenv("DB_PORT").toInt()
-//            dbName = System.getenv("DB_NAME")
-//            dbUser = System.getenv("DB_USER")
-//            dbPassword = System.getenv("DB_PASSWORD")
-//        }
+        val dbUrl = System.getenv("DATABASE_URL")
 
-        println("DB CONFIGG " + host)
-        println("DB CONFIGG " + port)
-        println("DB CONFIGG " + dbName)
-        println("DB CONFIGG " + dbUser)
-        println("DB CONFIGG " + dbPassword)
+        if (dbUrl != null) {
+            val dbUri = URI(dbUrl)
+            host = dbUri.host
+            port = dbUri.port
+            dbName = dbUri.path.substring(1)
+            val userInfo = dbUri.userInfo.split(":")
+            dbUser = userInfo[0]
+            dbPassword = userInfo[1]
+        } else {
+            host = System.getenv("DB_HOST")
+            port = System.getenv("DB_PORT").toInt()
+            dbName = System.getenv("DB_NAME")
+            dbUser = System.getenv("DB_USER")
+            dbPassword = System.getenv("DB_PASSWORD")
+        }
     }
 
     fun connect() = Database.connect(
